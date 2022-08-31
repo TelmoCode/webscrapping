4426824fe11659ce76956056806397f7a07cb747
TelmoCode <telmogferreira@gmail.com>
2022-08-31 17:47:20 -0300

    xpath

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import time
 class My_bot:
     def __init__(self):
         self.link_site = 'https://192.168.110.10:8001/portal/'
-        self.site_map = {}
+        self.site_map = {'avancado':'/html/body/div/div[2]/button[3]','ir_para':'/html/body/div/div[3]/p[2]/a'}
 
         self.driver = webdriver.Chrome(executable_path= 'C:\\chromedriver_win32\\chromedriver.exe')
         self.driver.maximize_window()
